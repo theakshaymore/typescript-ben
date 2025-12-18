7b4c019e065b9e803b1c1b2f45555baab5499c3b
Akshay More <moreakshay725@gmail.com>
2025-12-19 01:04:20 +0530

    npx tsc

---
+++ Base/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function run(name) {
+    return `Hello ${name}`;
+}
+console.log(run("akshay"));
+//# sourceMappingURL=index.js.map
---
+++ src/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(name) {
+    return `Hello ${name}, welcome to typescript`;
+}
+const username = "akshay";
+console.log(greet(username));
+//# sourceMappingURL=index.js.map
