888e4277da52c2079986210dd6ae9dcbcd84a331
Akshay More <moreakshay725@gmail.com>
2025-12-19 01:08:41 +0530

    remove unused TypeScript files and update tsconfig for output directory

--- Base/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function run(name) {
-    return `Hello ${name}`;
-}
-console.log(run("akshay"));
-//# sourceMappingURL=index.js.map
--- Base/index.ts
+++
@@ -1,5 +0,0 @@
-function run(name: string): string {
-  return `Hello ${name}`;
-}
-
-console.log(run("akshay"));
