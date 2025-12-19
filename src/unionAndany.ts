397a00440f619e7f814a15ba6b02bf003d24bb7a
Akshay More <moreakshay725@gmail.com>
2025-12-20 00:31:37 +0530

    for loop

--- src/unionAndany.ts
+++ src/unionAndany.ts
@@ -3,3 +3,14 @@ let subs: number | string = "1M";
 let apiRequestStatus: "pending" | "success" | "error" = "pending";
 
 apiRequestStatus = "error";
+
+const orders = ["12", "23", "45", "34"];
+
+let currentOrder;
+
+for (let order of orders) {
+  if (currentOrder === "45") {
+    currentOrder = order;
+    break;
+  }
+}
